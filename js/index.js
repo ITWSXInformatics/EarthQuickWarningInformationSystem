ac176a1f6fc64724bbb3b413e26effd61e9bb481
Andrew <adleaf1@gmail.com>
2020-04-22 02:08:07 -0400

    Started front end along with embedded map

---
+++ js/index.js
@@ -0,0 +1,10 @@
+function submit() {
+	var timeFrame = document.getElementById("timeFrame").value;
+	var rad = document.getElementById("radius").value;
+	var name = document.getElementById("earthquakeName").value;
+	
+	console.log(timeFrame);
+	console.log(rad);
+	console.log(name);
+	
+}
